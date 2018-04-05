d82a72e8eb7ee3ed637e15fc51984f074475b5b7
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-04 19:03:43 -0600

    fully functional, for reals this time

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let code;
 //on click, it listens then passes to the function
 $(".btn").on('click', function(){
 //the currency code assigned to the button pressed is now called code
-	code = $(".btn").val();
+	code = $(this).val();
 //this gets the data from the API
 		$.get("http://data.fixer.io/api/latest?access_key=4aa6c8dc93f5e3d4e9afffffd8fc4209", function(data){
 			data.rates[code];
